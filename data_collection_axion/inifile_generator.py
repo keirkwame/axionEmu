d55dba3549c02974c82a75497c4b56d4cb040561
Keir Rogers <keir.rogers@utoronto.ca>
2026-03-11 16:33:29 -0400

    Add bug fix test

--- data_collection_axion/inifile_generator.py
+++ data_collection_axion/inifile_generator.py
@@ -1,15 +1,15 @@
 import os
 # When run this script, make sure default_inifile_00 is in the same directory -- this is the default inifile for axionCAMB
 
-number_cores = 5 # this number should be consistent with number_cores variable in the 9parameters_data_collection_mp.py file and ideally num_subfile in LH_sampling.py
+number_cores = 21 # this number should be consistent with number_cores variable in the 9parameters_data_collection_mp.py file and ideally num_subfile in LH_sampling.py
 for i in range(number_cores):
-    new_folder = '/home/keir/keir/LH_ACT_DR6_TTTEEEPP_5high_axion_'+str(i)
+    new_folder = '/home/keir/keir/LH_ACT_DR6_TTTEEEPP_21_test_axion_'+str(i)
     os.system('mkdir ' + new_folder)
-    new_name = '/home/keir/keir/LH_ACT_DR6_TTTEEEPP_5high_axion_inifile_'+str(i)
+    new_name = '/home/keir/keir/LH_ACT_DR6_TTTEEEPP_21_test_axion_inifile_'+str(i)
     f1=open('default_inifile_00', 'r')
     lines = f1.readlines()
     f1.close()
-    lines[3] = 'output_root = '+ new_folder +'/LH_ACT_DR6_TTTEEEPP_5high_axion_'+str(i)+'\n'
+    lines[3] = 'output_root = '+ new_folder +'/LH_ACT_DR6_TTTEEEPP_21_test_axion_'+str(i)+'\n'
     os.system('touch '+new_name)
     f2 = open(new_name, 'w')
     f2.writelines(lines)
